194c41c5876e77a19caa9aa9131935435b24d92d
toto240325 <toto240325@gmail.com>
2021-06-09 22:36:22 +0200

    .

--- firstboot.sh
+++ firstboot.sh
@@ -5,4 +5,4 @@ then
   macadd=$( sed 's/://g' /sys/class/net/${macadd}/address )
   sed "s/raspberrypi/${macadd}/g" -i /etc/hostname /etc/hosts
 fi
-/sbin/shutdown -r 5 "reboot in five seconds"
+/sbin/shutdown -r 5 "reboot in five minutes"

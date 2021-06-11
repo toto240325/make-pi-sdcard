8b043e491847710f5afd4b028841d0c22f176024
toto240325 <toto240325@gmail.com>
2021-06-11 19:52:08 +0200

    .

--- firstboot.sh
+++ firstboot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-macadd=$( ip -brief add | awk '/UP/ {print $1}' | sort | head -1 )
+settings_file="make-pi-sdcard.ini"
+#macadd=$( ip -brief add | awk '/UP/ {print $1}' | sort | head -1 )
 if [ ! -z "${macadd}" ]
 then
   macadd=$( sed 's/://g' /sys/class/net/${macadd}/address )
   sed "s/raspberrypi/${macadd}/g" -i /etc/hostname /etc/hosts
 fi
+sed "s/raspberrypi/${myhostname}/g" -i /etc/hostname /etc/hosts
 /sbin/shutdown -r 5 "reboot in five minutes"
